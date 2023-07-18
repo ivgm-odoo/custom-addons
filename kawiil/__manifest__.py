942a4bc9514860a957ad7c251506d6a9fd9697d4
ivgm-odoo <ivgm@odoo.com>
2023-07-18 15:53:32 -0600

    TE02: Create a Module

---
+++ kawiil/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': 'Manage Registration of Motorcycles',
+    'description': """Motorcycle Registry
+    ====================
+    This Module is used to keep track of the Motorcycle
+    Registration and Ownership of each motorcycled of the brand.""",
+    'author': 'ivgm-odoo',
+    'website': 'https://github.com/ivgm-odoo/custom-addons',
+    'category': 'Kawiil/Kawiil',
+    'license': 'OPL-1',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
