df45f31b26af32b86f6b6700b5a546cb8697336f
ivgm-odoo <ivgm@odoo.com>
2023-07-20 15:59:46 -0600

    TE11: Inheritance - First part finished

--- kawiil/models/product_template.py
+++ kawiil/models/product_template.py
@@ -4,3 +4,7 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     detailed_type = fields.Selection(selection_add=[('motorcycle', 'Motorcycle'),], ondelete={'motorcycle': 'set product'})
+    type = fields.Selection(selection_add=[
+        ('motorcycle', 'Storable Product')
+    ], ondelete={'motorcycle': 'set consu'})
+    
