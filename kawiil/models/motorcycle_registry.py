10c0e3e2e78d74ce2c713ae4e33aebdc9b0304b6
ivgm-odoo <ivgm@odoo.com>
2023-07-20 11:31:46 -0600

    TE10: Advanced Fields

--- kawiil/models/motorcycle_registry.py
+++ kawiil/models/motorcycle_registry.py
@@ -9,8 +9,14 @@ class MotorcycleRegistry(models.Model):
 
     registry_number = fields.Char(string="Motorcycle Registry Number", required=True, default="MRN0001", copy=False, readonly=True)
     vin = fields.Char(required=True)
-    first_name = fields.Char(required=True)
-    last_name = fields.Char(required=True)
+    #first_name = fields.Char(required=True)
+    #last_name = fields.Char(required=True)
+    owner = fields.Many2one(comodel_name="res.partner", string="Partner", ondelete="cascade")
+    email = fields.Char(related="owner.email")
+    phone = fields.Char(related="owner.phone")
+    make = fields.Char(string="Make", compute="_compute_make")
+    model = fields.Char(string="Model", compute="_compute_model")
+    year = fields.Char(string="Year", compute="_compute_year")
     picture = fields.Image()
     current_mileage = fields.Float()
     license_plate = fields.Char()
@@ -38,4 +44,17 @@ class MotorcycleRegistry(models.Model):
             if(not validate):
                 raise ValidationError('The License Plate does not meet the definition rules')
             
-    
+    @api.depends('vin')
+    def _compute_make(self):
+        for rec in self: 
+            rec.make = "" if rec.vin==False else rec.vin[0:2]
+
+    @api.depends('vin')
+    def _compute_model(self):
+        for rec in self: 
+            rec.model = "" if rec.vin==False else rec.vin[2:4]
+
+    @api.depends('vin')
+    def _compute_year(self):
+        for rec in self: 
+            rec.year = "" if rec.vin==False else rec.vin[4:6]
